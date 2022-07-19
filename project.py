a4ea91e2893ca9eac61f2d552b8bcc57baa7c46f
zebra-spots <88425510+zebra-spots@users.noreply.github.com>
2022-07-19 09:55:27 +0100

    Create project.py

---
+++ project.py
@@ -0,0 +1,30 @@
+#! /usr/bin/python
+# Name: webscrape.py
+# Author: Chris
+# Revision v1.0
+# Description: This web scraper works on static HTML job sites
+# and will look for and print all python jobs.
+"""
+    Docstring: 
+
+Requires the following modules:
+
+python -m pip install requests
+python -m pip install beautifulsoup4
+"""
+import sys
+import requests
+from bs4 import BeautifulSoup
+
+def main():
+    """ main program overview """
+    
+    # Main program in here
+
+    
+    return None
+
+
+if __name__ == "__main__":
+    main()
+    sys.exit(0)
